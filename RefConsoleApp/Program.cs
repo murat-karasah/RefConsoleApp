862ec21ce8f026770fb57b7ba710a240216cc69a
Murat Karasah <mkarasah@gmail.com>
2021-12-03 10:56:02 +0300

    Add project files.

---
+++ RefConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RefConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=20, y=10;
+            degistir(ref x, ref y);
+            Console.WriteLine($"x in yeni deperi : {x}\ny'nin yeni değeri : {y}");
+        }
+       static void degistir(ref int a,ref int b)
+        {
+            int gecici = a;
+            a = b;
+            b = gecici;
+           
+        }
+    }
+}
